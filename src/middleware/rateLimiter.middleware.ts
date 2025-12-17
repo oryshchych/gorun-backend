16341826c5526d4c200ffe5b90a29881c577b97e
oryshchych <orishchich@gmail.com>
2025-12-17 14:26:33 +0200

    fix(middleware): Increase API request limit for rate limiter
    - Update the API rate limiter configuration to allow a maximum of 1000 requests per 15 minutes per IP, enhancing the capacity for handling higher traffic.

--- src/middleware/rateLimiter.middleware.ts
+++ src/middleware/rateLimiter.middleware.ts
@@ -21,7 +21,7 @@ export const authLimiter = rateLimit({
  */
 export const apiLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // Limit each IP to 100 requests per windowMs
+  max: 1000, // Limit each IP to 100 requests per windowMs
   message: 'Too many requests from this IP, please try again after 15 minutes',
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
